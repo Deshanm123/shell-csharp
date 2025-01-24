2dbd827b2bc895e65c22952be88a1d92371f839b
Deshanm123 <deshanm123@gmail.com>
2025-01-24 12:34:25 +0000

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -1,10 +1,10 @@
-using System.Net;
-using System.Net.Sockets;
+while (true)
+{
+    // Uncomment this line to pass the first stage
+    Console.Write("$ ");
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
-
-// Wait for user input
-var command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
+    // Wait for user input
+    var command = Console.ReadLine();
+    Console.WriteLine($"{command}: command not found");
+}
 
