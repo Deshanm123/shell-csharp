67a5907676d4fd6b6565e90f47ce3e96c3f869ec
Deshanm123 <deshanm123@gmail.com>
2025-01-31 15:09:00 +0000

    remove cat from shell words

--- src/main.cs
+++ src/main.cs
@@ -10,7 +10,7 @@ using static System.Net.Mime.MediaTypeNames;
 
 
 //shell built-in arr
-string[] shellKeyWordsArr = ["echo", "type", "exit", "pwd","cd","cat"];
+string[] shellKeyWordsArr = ["echo", "type", "exit", "pwd","cd"];
 
 //Get user path environments according to OS
 string[] GetPathDirectives()
